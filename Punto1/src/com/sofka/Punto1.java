355c9a6ae33597714fc25ff5356e4f3361e1da6a
MonicaOspina <monipoc@gmail.com>
2020-09-04 15:33:59 -0500

    test

--- Punto1/src/com/sofka/Punto1.java
+++ Punto1/src/com/sofka/Punto1.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Punto1 {
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         double cost;
 
@@ -14,7 +13,6 @@ public class Punto1 {
 
         System.out.print("Enter the number of days of stay: ");
         int stay = sc.nextInt();
-
         if ( distance >1000 && stay >7){
             cost = ((distance*35.00)-0.3*(distance*35.00));
             System.out.println("The cost of the ticket is: " +cost);
--- Punto2/src/com/sofka/Punto2.java
+++ Punto2/src/com/sofka/Punto2.java
@@ -3,84 +3,84 @@ package com.sofka;
 import java.util.*;
 
 public class Punto2 {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int option =0;
+        int option = 0;
         int weight, cost;
-        int flightReminingCapacity=18000;
-        int maxWeight=0;
-        int minWeight=flightReminingCapacity;
-        int amountOfPackages=0;
-        int totalCost=0;
-        int totalWeight=0;
-        int dollarPrice=3500;
+        int flightReminingCapacity = 18000;
+        int maxWeight = 0;
+        int minWeight = flightReminingCapacity;
+        int amountOfPackages = 0;
+        int totalCost = 0;
+        int totalWeight = 0;
+        int dollarPrice = 3500;
 
-        while (option!=3){
+        while (option != 3) {
             mostrarOpciones();
             option = sc.nextInt();
-            switch(option){
+            switch (option) {
                 case 1:
-                    System.out.println("Enter the weight for the package number: "+(amountOfPackages+1));
+                    System.out.println("Enter the weight for the package number: " + (amountOfPackages + 1));
                     weight = sc.nextInt();
-                    if(weight>flightReminingCapacity){
+                    if (weight > flightReminingCapacity) {
                         System.out.print("This package can´t be added as this would exceed the capacity of the plane");
-                    }if(weight>500){
-                    System.out.print("This package exceeds the maximum weight for one single package, please try with another weight.");
-                }else{
-                    //Sum the amount of packages loaded
-                    amountOfPackages++;
-                    totalWeight+=weight;
-
-                    //Compare if current package qualifies as max or min weight
-                    if(weight>maxWeight){
-                        maxWeight=weight;
                     }
-                    if(weight<minWeight){
-                        minWeight=weight;
-                    }
+                    if (weight > 500) {
+                        System.out.print("This package exceeds the maximum weight for one single package, please try with another weight.");
+                    } else {
+                        //Sum the amount of packages loaded
+                        amountOfPackages++;
+                        totalWeight += weight;
 
-                    //Calculate package cost
-                    if (weight>=0 && weight<=25) {
-                        cost=0;
-                        System.out.println("This package is cover free as it does not exceeds 25 Kg");
-                        totalCost = totalCost+cost;
-                    }else if(weight>=26 &&  weight<=300){
-                        cost= weight*1500;
-                        System.out.println("The cost for this package is: " +cost);
-                        totalCost = totalCost+cost;
+                        //Compare if current package qualifies as max or min weight
+                        if (weight > maxWeight) {
+                            maxWeight = weight;
+                        }
+                        if (weight < minWeight) {
+                            minWeight = weight;
+                        }
 
-                    }else if(weight>=301 &&  weight<=500){
-                        cost= weight*2500;
-                        System.out.println("The cost for this package is: "+cost);
-                        totalCost = totalCost+cost;
+                        //Calculate package cost
+                        if (weight >= 0 && weight <= 25) {
+                            cost = 0;
+                            System.out.println("This package is cover free as it does not exceeds 25 Kg");
+                            totalCost = totalCost + cost;
+                        } else if (weight >= 26 && weight <= 300) {
+                            cost = weight * 1500;
+                            System.out.println("The cost for this package is: " + cost);
+                            totalCost = totalCost + cost;
+
+                        } else if (weight >= 301 && weight <= 500) {
+                            cost = weight * 2500;
+                            System.out.println("The cost for this package is: " + cost);
+                            totalCost = totalCost + cost;
+                        }
+                        System.out.println("The total weight is: " + totalWeight);
                     }
-                    System.out.println("The total weight is: "+totalWeight);
-                }
                     break;
 
                 case 2:
-                    System.out.println("The total weight is: "+totalWeight);
-                    System.out.println("The flight has a total of: "+amountOfPackages+" packages.");
-                    System.out.println("The heaviest package is "+maxWeight+" kilos.");
-                    System.out.println("The lightweight package is "+minWeight+" kilos.");
+                    System.out.println("The total weight is: " + totalWeight);
+                    System.out.println("The flight has a total of: " + amountOfPackages + " packages.");
+                    System.out.println("The heaviest package is " + maxWeight + " kilos.");
+                    System.out.println("The lightweight package is " + minWeight + " kilos.");
                     System.out.println("The amount collected from packages is:");
-                    System.out.println("            $"+totalCost+" pesos.");
-                    System.out.println("         or $"+totalCost/dollarPrice+" dollars.");
-                    System.out.println("Averge weight per package: "+totalWeight/amountOfPackages);
+                    System.out.println("            $" + totalCost + " pesos.");
+                    System.out.println("         or $" + totalCost / dollarPrice + " dollars.");
+                    System.out.println("Averge weight per package: " + totalWeight / amountOfPackages);
 
-                    option=3;
+                    option = 3;
                     System.out.println("Good Bye!");
                     break;
                 case 3:
                     System.out.println("Good Bye!");
                     break;
                 default:
-                    System.out.println(option+" is not an avalible option, please try again.");
+                    System.out.println(option + " is not an avalible option, please try again.");
             }
         }
 
     }
-
     private static void mostrarOpciones(){
         System.out.println("Enter the desired option:");
         System.out.println("1. Add new Package");
